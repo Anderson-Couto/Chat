31fdcfc242cc5b6f0b4bd9206b4555197dbdd24f
Anderson Couto Cardoso <andersoncouto57@gmail.com>
2020-06-11 10:09:32 -0300

    v0.3

--- src/settings.py
+++ src/settings.py
@@ -121,3 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("localhost", 6379)],
+        },
+    },
+}
